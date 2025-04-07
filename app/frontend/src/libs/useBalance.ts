d678ac6a81da0b79c61dab1614853cc48bfe344b
Benjamin <58327@novasbe.pt>
2025-04-07 23:47:16 +0200

    Globally configure axios & simplify usage

--- app/frontend/src/libs/useBalance.ts
+++ app/frontend/src/libs/useBalance.ts
@@ -1,9 +1,16 @@
-import axios from 'axios';
+import { getCurrentInstance } from 'vue';
 
 export function useBalance() {
+    const { proxy } = getCurrentInstance() as any;
+
+    /**
+     * Fetches the current user balance from the backend
+     */
     const getBalance = async () => {
         try {
-            return (await axios.get('http://localhost:8000/api/balances')).data.balance;
+            // This works because $api is globally configured to prepend urls with
+            // https://localhost:8000/api (URL of the backend, see main.js)
+            return (await proxy.$api.get('/balances')).data.balance;
         } catch (err) {
             console.error('An error occurred while fetching the balance.');
             return 0;
